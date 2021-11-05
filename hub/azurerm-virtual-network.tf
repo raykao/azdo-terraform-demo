04879f94e14afe70708d3702b8fc861f864fc857
Ray Kao <ray@peopleandcode.com>
2021-11-05 11:04:57 -0400

    adding in default subnet

--- hub/azurerm-virtual-network.tf
+++ hub/azurerm-virtual-network.tf
@@ -4,3 +4,12 @@ resource "azurerm_virtual_network" "hub" {
 	location = azurerm_resource_group.primary.location
 	address_space = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "default" {
+	name = "DefaultSubnet"
+	resource_group_name = azurerm_resource_group.primary.name
+	virtual_network_name = azurerm_virtual_network.hub.name
+	address_prefixes = [
+		"10.0.0.0/24"
+	]
+}

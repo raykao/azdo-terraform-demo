7e592597b2a5571843678742a423efdf4da150f0
Ray Kao <ray@peopleandcode.com>
2021-11-05 00:13:47 -0400

    forgot the properties

--- hub/azurerm-virtual-network.tf
+++ hub/azurerm-virtual-network.tf
@@ -1,5 +1,6 @@
 resource "azurerm_virtual_network" "hub" {
 	name = "hub-vent"
+	resource_group_name = azurerm_resource_group.primary.name
 	location = azurerm_resource_group.primary.location
-	address_space = "10.0.0.0/16"
+	address_space = ["10.0.0.0/16"]
 }
